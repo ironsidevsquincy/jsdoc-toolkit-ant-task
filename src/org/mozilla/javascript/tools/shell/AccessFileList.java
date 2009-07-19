7c1974a37913bdf1a2b1008b3a8c274fd12fe59a
Darren Hurley <ironsidevsquincy@googlemail.com>
2009-07-19 17:40:43 +0000

    Issue 9 - Added latest rhino jar

---
+++ src/org/mozilla/javascript/tools/shell/AccessFileList.java
@@ -0,0 +1,16 @@
+/**
+ * Just a simple class in the same package as org.mozilla.javascript.tools.shell, giving it 
+ * access, through a static class, to the default static field fileList in the 
+ * org.mozilla.javascript.tools.shell.Main class.
+ * 
+ */
+
+package org.mozilla.javascript.tools.shell;
+
+import org.mozilla.javascript.tools.shell.Main;
+
+public class AccessFileList {
+	public static void clearFileList(){
+		Main.fileList.clear();		
+	}
+}
